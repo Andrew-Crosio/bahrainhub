bd8892fb9a2fa89faaf4d52bad863a83fe9eeeb8
gxx <crosio@dubizzle.com>
2012-09-27 03:48:22 +0400

    changed to image uploader, convert to png, removed whitelist (doesn't work for some things like php files)

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -1,17 +1,15 @@
 class PostImageUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::MiniMagick
+  include CarrierWave::MimeTypes
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :fog
+  process :set_content_type
+  process :convert => 'png'
+  storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     'https://s3.amazonaws.com/crowdvoice-production/link-default.png'
   end
@@ -29,15 +27,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
     model.image_width, model.image_height = *img[:dimensions]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
+  def filename
+    super.chomp(File.extname(super)) + '.png'
   end
-
-  # Override the filename of the uploaded files:
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
 end
